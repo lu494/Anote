d9ac23d9c1ea248992ebd400d3df21fff48ec852
lu494 <liu71791@gmail.com>
2026-03-28 11:06:18 +0800

    修改vuetify.ts

--- front_end/src/plugins/vuetify.ts
+++ front_end/src/plugins/vuetify.ts
@@ -14,6 +14,22 @@ import 'vuetify/styles'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: 'lightYellow',
+    themes: {
+      lightYellow: {
+        dark: false,
+        colors: {
+          primary: '#F5E6D3',
+          secondary: '#FDF8F0',
+          accent: '#D9B48B',
+          background: '#FDF7F0',
+          surface: '#FFFFFF',
+          error: '#E57373',
+          info: '#64B5F6',
+          success: '#81C784',
+          warning: '#FFB74D',
+        },
+      },
+    },
   },
 })
